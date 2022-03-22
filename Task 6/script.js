602b55d145f22d26ba868ffcc787e349781b66a2
kiburtaite <kiburtaite@gmail.com>
2022-03-22 09:55:19 +0200

    uzduotis 6

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,15 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+let getUserAverage = (array) => {
+  let year = (array.map(array => array.age));
+  return (year.reduce((a,v) => (a+v)))/year.length
+};
+
+let getUsersNames = (array) => {
+  return array.map(array => array.name)
+};
+
+console.log(getUserAverage(users));
+console.log(getUsersNames(users));
