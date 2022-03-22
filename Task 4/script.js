653cfcfd5a80450479ea506f55efd009a11f7eaa
kiburtaite <kiburtaite@gmail.com>
2022-03-22 11:43:42 +0200

    uzduotis 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,16 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+.then(res => res.json())
+//.then(cars => console.log(cars))
+.then(cars => show(cars))
+
+let show = (data) => {
+    data.forEach(data => {
+        document.querySelector("#output").innerHTML +=
+        `<h2>${data.brand}</h2>
+        <p>${data.models}</p>`
+    })
+}
